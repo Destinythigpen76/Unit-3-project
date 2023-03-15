2aa408c3f54b4c9b352fcf0ef2d91e90eb69fe9e
Destinythigpen76 <127964239+Destinythigpen76@users.noreply.github.com>
2023-03-15 09:19:18 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+ 
+let firstButton=document.querySelector(".option-one");
+let secondButton=document.querySelector(".option-two");
+let answerOne=document.querySelector(".option-one-screen");
+let answerTwo=document.querySelector(".option-two-screen");
+let endingOne=document.querySelector(".option-one-end");
+let endingTwo=document.querySelector("option-two-end");
+
+
+ 
+firstButton.onclick=function(){
+answerOne.style.display="block";
+};
+
+secondButton.onclick=function(){
+answerTwo.style.display="block";
+};
+
+
+answerOne.onclick=function(){
+
+};
+
+
+
+
